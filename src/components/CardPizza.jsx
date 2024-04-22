ad50191744a5a272c48fa16172603fbeb8dba856
Rodrigo Rivas <rodroy019@hotmail.com>
2024-04-21 23:46:10 -0400

    add: se agregan las vistas y componentes

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,6 +1,25 @@
+import { Card, ListGroup, Button } from 'react-bootstrap'
+
 const CardPizza = () => {
   return (
-    <div>CardPizza</div>
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant='top' src='' />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+      </Card.Body>
+      <ListGroup className='list-group-flush'>
+        <ListGroup.Item>
+          Ingredientes:
+        </ListGroup.Item>
+        <ListGroup.Item>
+          Precio $
+          <div>
+            <Button variant='primary'>Ver Mas</Button>
+            <Button variant='danger'>Agregar</Button>
+          </div>
+        </ListGroup.Item>
+      </ListGroup>
+    </Card>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,25 @@
+import { Container, Nav, Navbar } from 'react-bootstrap'
+// import { NavLink } from 'react-router-dom'
+
 const NavBar = () => {
+  // const HandleMenu = ({ isActive }) => isActive ? 'nav-item nav-link avtive' : 'nav-item nav-link'
+
   return (
-    <div>NavBar</div>
+    <Navbar expand='lg' bg='primary' data-bs-theme='dark'>
+      <Container>
+        <Navbar.Brand href=''>
+          <img src='' height='40' className='d-inline-block align-top' alt='LogoPizza' />
+          {' '} Pizzas Rya
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ms-auto'>
+            <img src='' height='40' className='d-inline-block' alt='Logo' />
+            <p>Total: $ XXXXX</p>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   )
 }
 
